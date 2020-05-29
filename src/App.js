e772c1c17539a9462e4b3954a82124082b18855e
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-29 16:28:36 -0600

    create better style

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ constructor() {
   }
 
   this.componentDidMount = this.componentDidMount.bind(this);
+  this.addProuduct = this.addProuduct.bind(this);
+  this.delete = this.delete.bind(this);
 }
 
 componentDidMount() {
@@ -23,26 +25,29 @@ componentDidMount() {
   }).catch(error => alert("Didn't get a inventory back."))
 }
 
+addProuduct(body) {
+  axios.post('/api/product', body).then(response => {
+    this.setState({inventory: response.data})
+  })
+ }
 
-// delete(id) {
-//   console.log(id)
-//   axios.delete(`/api/inventory/${id}`).then(response => {
-//     this.setState({inventory: response.data})
-//   }).catch(error => alert("No Book was found to remove"))
-// }
+delete(id) {
+  console.log(id)
+  axios.delete(`/api/inventory/${id}`).then(response => {
+    this.setState({inventory: response.data})
+  }).catch(error => alert("No Book was found to remove"))
+}
 
 
   render() {
     console.log(this.state.inventory)
     return (
       <div className="App">
-        <h1>App main Component</h1>
-
+        <Header />
         <Dashboard 
           inventory={this.state.inventory}
         />
         <Form />
-        <Header />
       </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,11 @@ class Header extends Component {
   render() {
     return (
       <div className="App">
-        <div>Header Component</div>
+        <div className='nav-container'>
+          <img src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fshelfie.png?v=1590790218504" alt="logo"/>
+          <button>Dashboard</button>
+          <button>Add Inventory</button>
+        </div>
       </div>
     );
   }
